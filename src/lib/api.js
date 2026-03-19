a22a6f84a4a59efa764902d61c98d79494880fc7
Essy116 <githinjie40@gmail.com>
2026-03-19 16:50:57 +0300

    feat: add API queries and mutations

--- src/lib/api.js
+++ src/lib/api.js
@@ -13,6 +13,36 @@ export const signInMutationFn = async (data) => {
   return response.data;
 };
 
+// export const signInMutationFn = async (data) => {
+//   const response = await API.post("/identity/sign-in", data);
+//   return response.data;
+// };
+
+export const GetBasketQueryFn = async (data) => {
+  const response = await API.get("/basket", data);
+  return response.data;
+};
+
+export const AddItemBasketMutationFn = async (data) => {
+  const response = await API.post("/basket/items", data);
+  return response.data;
+};
+
+export const RemoveItemBasketMutationFn = async (productId) => {
+  const response = await API.delete(`/basket/items/${productId}`);
+  return response.data;
+};
+
+export const UpdateItemQuantityMutationFn = async (data) => {
+  const response = await API.put("/basket/items", data);
+  return response.data;
+};
+
+export const CheckoutBasketMutationFn = async (data) => {
+  const response = await API.post("/basket/checkout", data);
+  return response.data;
+};
+
 export const getCurrentUserQueryFn = async () => {
   const response = await API.get("/identity/current-client");
   return response.data;
@@ -62,5 +92,21 @@ export const getCategoriesHierarchyQueryFn = async () => {
   const response = await API.get("/categories/hierarchy", {
     withCredentials: false,
   });
+  return response.data;
+};
+export const DeleteBasketMutationFn = async (data) => {
+  const response = await API.delete("/basket", data);
+  return response.data;
+};
+
+export const getOrderByIdQueryFn = async (orderId) => {
+  const response = await API.get(`/orders/${orderId}`);
+  return response.data;
+};
+
+export const getOrdersByCustomerQueryFn = async ({ customerId, pageIndex = 0, pageSize = 10 }) => {
+  const response = await API.get(
+    `/orders/customer/${customerId}?PageIndex=${pageIndex}&PageSize=${pageSize}`
+  );
   return response.data;
 };
--- src/lib/axios-client.js
+++ src/lib/axios-client.js
@@ -4,8 +4,8 @@ const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 const options = {
 	baseURL,
-	withCredentials: true,
-	timeout: 10000,
+	// withCredentials: true,
+	// timeout: 10000,
 };
 
 const API = axios.create(options);
