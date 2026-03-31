c7fdf135431116740300377d2ec0dcd877ca90ad
Essy116 <githinjie40@gmail.com>
2026-03-31 06:28:53 +0300

    feat(homepage): add new sections to homepage

--- src/components/Checkout/PayNow.jsx
+++ src/components/Checkout/PayNow.jsx
@@ -3,40 +3,43 @@ import { useSelector } from "react-redux";
 import { useMutation } from "@tanstack/react-query";
 import { toast } from "../../hooks/use-toast";
 import { Link, useNavigate } from "react-router-dom";
-
 import {
   InitiatePaymentMutationFn,
   postConfirmMpesaPaymentMutationFn,
 } from "../../lib/api";
 import { selectCardInfo } from "../../features/checkoutSlice";
-import { selectCheckoutResponse, selectCheckoutPayload } from "../../features/checkoutSlice";
-import MpesaIcon from "../../assets/images/logo/Mpesa-Logo.png"; // path to your icon
+import {
+  selectCheckoutResponse,
+  selectCheckoutPayload,
+  selectCheckoutItems,
+} from "../../features/checkoutSlice";
+import MpesaIcon from "../../assets/images/logo/Mpesa-Logo.png"; 
 const PayNow = ({ closePayNow }) => {
-  // Get checkout data from Redux
   const { type, last4, name } = useSelector(selectCardInfo);
-const checkoutResponse = useSelector(selectCheckoutResponse);
-const checkoutPayload = useSelector(selectCheckoutPayload);
+  const checkoutResponse = useSelector(selectCheckoutResponse);
+  const { cart } = useSelector((state) => state.itemCart);
+  const items = useSelector(selectCheckoutItems);
+  const checkoutPayload = useSelector(selectCheckoutPayload);
   const navigate = useNavigate();
-const maskPhoneNumber = (phone) => {
-  if (!phone) return "N/A";
-  const last4 = phone.slice(-4);
-  return `XXXX-${last4}`;
-};
+  const maskPhoneNumber = (phone) => {
+    if (!phone) return "N/A";
+    const last4 = phone.slice(-4);
+    return `XXXX-${last4}`;
+  };
 
   const paymentMethodMap = {
-  1: "M-PESA",
-  2: "Card",
-  3: "Paybill",
-  4: "Cash on Delivery",
-};
+    1: "M-PESA",
+    2: "Card",
+    3: "Paybill",
+    4: "Cash on Delivery",
+  };
 
-const email = checkoutPayload?.shippingAddress?.emailAddress;
-// const phone = checkoutPayload?.shippingAddress?.phoneNumber;
-// const paymentMethod = checkoutPayload?.payment?.method;
-const date = checkoutResponse?.createdAt;
- const selectedMethod =
-  paymentMethodMap[checkoutPayload?.payment?.method] || "N/A";
-
+  const email = checkoutPayload?.shippingAddress?.emailAddress;
+  // const phone = checkoutPayload?.shippingAddress?.phoneNumber;
+  // const paymentMethod = checkoutPayload?.payment?.method;
+  const date = checkoutResponse?.createdAt;
+  const selectedMethod =
+    paymentMethodMap[checkoutPayload?.payment?.method] || "N/A";
 
   const formatCurrency = (amount) =>
     new Intl.NumberFormat("en-KE", {
@@ -46,9 +49,9 @@ const date = checkoutResponse?.createdAt;
 
   const total = formatCurrency(checkoutResponse.totalAmount || 0);
 
-const formattedDate = date
-  ? new Date(date).toLocaleDateString("en-KE")
-  : "N/A";  
+  const formattedDate = date
+    ? new Date(date).toLocaleDateString("en-KE")
+    : "N/A";
   // --- Initiate Payment Mutation ---
   const { mutate: initiatePayment, isLoading: isInitiating } = useMutation({
     mutationFn: () => InitiatePaymentMutationFn(checkoutResponse.orderId),
@@ -58,9 +61,9 @@ const formattedDate = date
         description: "You can now confirm your M-PESA payment.",
         variant: "success",
       });
-      
-        navigate("/confirm");
-   
+ setTimeout(() => {
+      navigate("/confirm");
+      }, 5000);
     },
     onError: (err) => {
       toast({
@@ -71,10 +74,7 @@ const formattedDate = date
     },
   });
 
-
-
   const handleInitiatePayment = () => initiatePayment();
-
 
   // // If checkout data not ready
   // if (!checkoutResponse?.isSuccess) {
@@ -82,101 +82,130 @@ const formattedDate = date
   // }
 
   return (
- <div
-  className="container my-5"
-  style={{ maxWidth: "800px", width: "100%" }} 
->
-  <h2 className="text-center mb-3 fw-bold">Order received</h2>
-  <p className="text-center text-muted mb-4">
-    Thank you. Your order has been received.
-  </p>
-  <h6 className="mb-3 fw-semibold ">Order Summary</h6>
-<div
-  className="border rounded p-3 mb-4"
-  style={{ borderColor: "#663300", fontSize: "14px" }}
->
+    <section className="checkout py-40">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-12 col-md-10 col-lg-8">
+            <div className="text-center">
+              <h3>Order Received.</h3>
+            </div>
 
+            <p className="text-dark ">
+              Thank you. Your Order has been received.
+            </p>
 
-  <div className="row text-center text-lg-start mb-3">
-      <div className="col">
-      <small className="text-muted d-block">Order Number</small>
-      <span className="fw-semibold"></span>
-    </div>
-    <div className="col">
-      <small className="text-muted d-block">DATE</small>
-      <span className="fw-semibold">{formattedDate}</span>
-    </div>
-    <div className="col">
-      <small className="text-muted d-block">EMAIL</small>
-      <span className="fw-semibold">{email}</span>
-    </div>
-    <div className="col">
-      <small className="text-muted d-block">PAYMENT METHOD</small>
-      <span className="fw-semibold">{selectedMethod}</span>
-    </div>
-  </div>
+            {/* ===== SUMMARY CARD ===== */}
+            <div className="card mb-4">
+              <div className="card-body">
+                <div className="row text-center text-sm">
+                  {/* Row 1: Order Number + Date + Payment Method */}
+                  <div className="col-12 col-sm-4 mb-3 d-flex flex-column align-items-start border-end pe-3">
+                    <p className="mb-1">Order Number:</p>
+                    {/* <span className="fw-semibold">{checkoutResponse?.checkoutId || "—"}</span> */}
+                  </div>
 
- 
-  <div className="row text-center text-lg-start">
-    <div className="col">
-      <small className="text-muted d-block">SUBTOTAL</small>
-      <span className="fw-semibold">{formatCurrency(checkoutResponse.subTotal)}</span>
-    </div>
-    <div className="col">
-      <small className="text-muted d-block">DELIVERY FEE</small>
-      <span className="fw-semibold">{formatCurrency(checkoutResponse.deliveryFee)}</span>
-    </div>
-    <div className="col">
-      <small className="text-muted d-block">VAT</small>
-      <span className="fw-semibold">{formatCurrency(checkoutResponse.vatAmount)}</span>
-    </div>
-    <div className="col">
-      <small className="text-muted d-block">TOTAL</small>
-      <span className="fw-bold">{formatCurrency(checkoutResponse.totalAmount)}</span>
-    </div>
-  </div>
-</div>
-  <h6 className="mb-3 fw-semibold "> Payment Method</h6>
+                  <div className="col-12 col-sm-4 mb-3 d-flex flex-column align-items-start border-end pe-3">
+                    <p className="mb-1 text-sm">Date:</p>
+                    <span className="fw-semibold text-sm">{formattedDate}</span>
+                  </div>
 
-  <div className="d-flex flex-column align-items-center text-center">
-    <div className="border rounded p-3 mb-4 d-flex justify-content-between align-items-center w-100">
-      <div className="d-flex align-items-center gap-2">
-        <input type="radio" checked readOnly />
-        <span className="fw-medium">{selectedMethod || "N/A"}</span>
+                  <div className="col-12 col-sm-4 mb-3 d-flex flex-column align-items-start border-end pe-3">
+                    <p className="mb-1">Payment Method:</p>
+                    <span className="fw-semibold">{selectedMethod}</span>
+                  </div>
 
-        {selectedMethod === "M-PESA" &&
-          checkoutPayload?.payment?.phoneNumber && (
-            <small className="fw-medium ms-2">
-              {maskPhoneNumber(checkoutPayload.payment.phoneNumber)}
-            </small>
-          )}
+                  {/* Row 2: Email + Total */}
+                  <div className="col-12 col-sm-4 mb-3 d-flex flex-column align-items-start border-end pe-3">
+                    <p className="mb-1 text-sm">Email:</p>
+                    <span className="fw-semibold text-sm">{email || "—"}</span>
+                  </div>
+
+                  <div className="col-12 col-sm-4 mb-3 d-flex flex-column align-items-start border-end pe-3">
+                    <p className="mb-1 text-sm">Total:</p>
+                    <span className="fw-semibold text-sm">
+                      {new Intl.NumberFormat("en-KE", {
+                        style: "currency",
+                        currency: "KES",
+                      }).format(checkoutResponse?.totalAmount || 0)}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="order-details my-4">
+              <h3>Order Details</h3>
+
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item d-flex justify-content-between fw-semibold text-sm">
+                  <span>Product</span>
+                  <span>Total</span>
+                </li>
+
+                {items?.map((item) => (
+                  <li
+                    key={item.productId}
+                    className="list-group-item d-flex justify-content-between"
+                  >
+                    <span className="text-sm ">
+                      {item.productName} <em>(x {item.quantity})</em>
+                    </span>
+                    <span className="text-sm fw-semibold">
+                      {formatCurrency(item.unitPrice * item.quantity)}
+                    </span>
+                  </li>
+                ))}
+
+                <li className="list-group-item d-flex justify-content-between">
+                  <span className="text-sm ">Subtotal:</span>
+                  <span className="text-sm fw-semibold">
+                    {formatCurrency(checkoutResponse?.subTotal)}
+                  </span>
+                </li>
+
+                <li className="list-group-item d-flex justify-content-between">
+                  <span className="text-sm ">Delivery fee:</span>
+                  <span className="text-sm fw-semibold">
+                    {formatCurrency(checkoutResponse?.deliveryFee)}
+                  </span>
+                </li>
+
+                <li className="list-group-item d-flex justify-content-between">
+                  <span className="text-sm ">Total:</span>
+                  <span className="text-sm fw-semibold">
+                    {formatCurrency(checkoutResponse?.totalAmount)}
+                  </span>
+                </li>
+              </ul>
+
+              {/* ===== PAYMENT BUTTON ===== */}
+       <div className="d-flex justify-content-center mt-4">
+  <button
+    type="button"
+    className="btn btn-sm btn-main mt-4 py-3 w-25 rounded-3 d-flex align-items-center justify-content-between"
+    disabled={isInitiating}
+    onClick={handleInitiatePayment}
+  >
+    {isInitiating && (
+      <div className="spinner-border spinner-border-sm me-2" role="status">
+        <span className="visually-hidden">Loading...</span>
       </div>
+    )}
 
-      <div>
-        {selectedMethod === "M-PESA" ? (
-          <img src={MpesaIcon} alt="M-PESA" className="w-40" />
-        ) : (
-          <small className="text-success fw-bold">
-            {selectedMethod || "N/A"}
-          </small>
-        )}
-      </div>
-    </div>
+    {/* Centered Text */}
+    <span className="mx-auto">Pay Now</span>
 
-    <div className="d-flex gap-2 w-100">
-      <button
-        className="btn btn-main-two flex-grow-1 py-3 fw-semibold"
-        onClick={handleInitiatePayment}
-        nav
-        disabled={isInitiating}
-      >
-        {isInitiating
-          ? "Initiating Payment..."
-          : `Initiate Payment: ${total}`}
-      </button>
-    </div>
-  </div>
+    {/* Image on the right */}
+    {selectedMethod === "M-PESA" && (
+      <img src={MpesaIcon} alt="mpesa" width={40} />
+    )}
+  </button>
 </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
